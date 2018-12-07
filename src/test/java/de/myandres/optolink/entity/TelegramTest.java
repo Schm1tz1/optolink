96caf48fa93583e4d4a511cee89ac62ec937523d
Roman Schmitz <Schm1tz1@github.com>
2018-12-07 10:11:39 +0100

    added dummy test class

---
+++ src/test/java/de/myandres/optolink/entity/TelegramTest.java
@@ -0,0 +1,31 @@
+package de.myandres.optolink.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.*;
+
+public class TelegramTest {
+
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+
+    @Test
+    void firstTest() {
+        assertEquals(2, 1+1);
+    }
+
+}
